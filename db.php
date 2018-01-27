3e104ff8c1ccbba51dd5ec147e3e5684a628f7ed
Nancy Snapper <nsnapper@spoontiques.com>
2018-01-27 15:39:33 -0500

    Changed db.php so it can run locally and on Heroku

--- db.php
+++ db.php
@@ -1,21 +1,23 @@
 <?php
 
 //mysql://b7a7e5ee9c955a:8c71d871@us-cdbr-iron-east-05.cleardb.net/heroku_aeeec5f573ac96d?reconnect=true
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-//
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-$connection = new mysqli($server, $username, $password, $db);
-
+if (getenv("CLEARDB_DATABASE_URL") != null) {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    //
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+    $connection = new mysqli($server, $username, $password, $db);
+} else {
 
 // LOCALHOST settings:
-//$db['db_host'] = "localhost";
-//$db['db_user'] = "root";
-//$db['db_pass'] = "root";
-//$db['db_name'] = "spoontiqueswebsite";
-
+    $db['db_host'] = "localhost";
+    $db['db_user'] = "root";
+    $db['db_pass'] = "root";
+    $db['db_name'] = "spoontiqueswebsite";
+    $connection = mysqli_connect($db['db_host'], $db['db_user'], $db['db_pass'], $db['db_name']);
+}
 //foreach($db as $key => $value){
     
 //    define(strtoupper($key), $value);
