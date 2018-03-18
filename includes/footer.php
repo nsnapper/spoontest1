090e760743f8246427ba42aa8a6fd31227625a00
Nancy Snapper <nsnapper@spoontiques.com>
2018-03-18 17:21:01 -0400

    fax number again

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <div id="footer">
-    <p class="footer">111 Island St., Stoughton, MA  02072    Phone: 800-225-5826    Fax: 731-344-1576</p>
+    <p class="footer">111 Island St., Stoughton, MA  02072    Phone: 800-225-5826    Fax: 781-344-1576</p>
     <p class="footer">Copyright © 2018 Spoontiques. All Rights Reserved.</p>
 </div>
