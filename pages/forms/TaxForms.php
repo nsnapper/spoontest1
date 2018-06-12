961b9eefc806d6ace7278f59411b32a3e87129e1
Nancy Snapper <nsnapper@spoontiques.com>
2018-06-11 22:21:01 -0400

    forms

---
+++ pages/forms/TaxForms.php
@@ -0,0 +1,34 @@
+<html lang="en">
+
+<head>
+    <title>Spoontiques, Inc. - Wholesale Giftware</title>
+    <link rel="stylesheet" type="text/css" href="../../css/Spoontiques.css">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+</head>
+
+
+<body>
+    <?php include "../../includes/logo.php"; ?>
+
+     
+<div id="pdf_lists">
+    <ul class="list-group">
+
+        <li class="list-group-item"><a href='documents/D2002-Uniform-Sales-and-Use-Certificate-5-2016a-Spoontiques.pdf'>Multi-State Sales and Use Certificate</a></li>
+        <li class="list-group-item"><a href='documents/D2007-DC-2012_otr_368_fill-in.pdf'>District of Columbia Form OTR 368</a></li>
+        <li class="list-group-item"><a href='documents/D2008-IN-ST-105.pdf'>Indiana Form ST-105</a></li>
+        <li class="list-group-item"><a href='documents/D2004-MA-dor-sales-use-form-st-4.pdf'>Massachusetts Form ST-4</a></li>
+        <li class="list-group-item"><a href='documents/D2003-MI-3372_216612_7.pdf'>Michigan Form 3372</a></li>
+   </ul>    
+   
+</div>
+
+<div>
+    <?php include "../../includes/footer.php";  ?>
+</div>
+</body>
---
+++ pages/forms/index.php
@@ -0,0 +1,30 @@
+<html lang="en">
+
+<head>
+    <title>Spoontiques, Inc. - Wholesale Giftware</title>
+    <link rel="stylesheet" type="text/css" href="../../css/Spoontiques.css">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+</head>
+
+
+<body>
+    <?php include "../../includes/logo.php"; ?>
+
+     
+<div id="pdf_lists">
+    <ul class="list-group">
+        <li class="list-group-item"><a href='TaxForms.php'>State Tax Forms</a></li>
+        <li class="list-group-item"><a href='documents/CreditApplication.pdf'>Credit Application</a></li>
+   </ul>    
+   
+</div>
+
+<div>
+    <?php include "../../includes/footer.php";  ?>
+</div>
+</body>
