96098b9e5b249b956fb7f85716989fca23bd566c
Nancy Snapper <nsnapper@spoontiques.com>
2018-09-03 10:12:09 -0400

    July 2018

--- pages/pdfs/CanadianLicenses.php
+++ pages/pdfs/CanadianLicenses.php
@@ -24,7 +24,8 @@
         <li class="list-group-item"><a href='documents/CanadianDeanRusso.pdf'><img width='100' src='images/DeanRusso.jpg' alt='images'>Dean Russo</a></li>
         <li class="list-group-item"><a href='documents/CanadianElvis.pdf'><img width='100' src='images/Elvis.jpg' alt='images'>Elvis Presley</a></li>
         <li class="list-group-item"><a href='documents/CanadianFord.pdf'><img width='100' src='images/Ford.jpg' alt='images'>Ford</a></li>
-        <li class="list-group-item"><a href='documents/CanadianILoveLucy.pdf'><img width='100' src='images/ILoveLucy.jpg' alt='images'>I Love Lucy</a></li>
+        <li class="list-group-item"><a href='documents/HarryPotterCanada.pdf'><img width='100' src='images/HarryPotter.jpg' alt='images'>Harry Potter</a></li>
+        <li class="list-group-item"><a href='documents/HolidayMoviesCanada.pdf'><img width='100' src='images/HolidayMovies.jpg' alt='images'>Holiday Movies</a></li> <li class="list-group-item"><a href='documents/CanadianILoveLucy.pdf'><img width='100' src='images/ILoveLucy.jpg' alt='images'>I Love Lucy</a></li>
         <li class="list-group-item"><a href='documents/CanadianLooneyTunes.pdf'><img width='100' src='images/LooneyTunes.jpg' alt='images'>Looney Tunes</a></li>
         <li class="list-group-item"><a href='documents/CanadianRadioDays.pdf'><img width='100' src='images/RadioDays.jpg' alt='images'>Radio Days</a></li>
         <li class="list-group-item"><a href='documents/CanadianScoobyDoo.pdf'><img width='100' src='images/ScoobyDoo.jpg' alt='images'>Scooby Doo</a></li>
--- pages/pdfs/licenses.php
+++ pages/pdfs/licenses.php
@@ -24,7 +24,8 @@
         <li class="list-group-item"><a href='documents/DeanRusso.pdf'><img width='100' src='images/DeanRusso.jpg' alt='images'>Dean Russo</a></li>
         <li class="list-group-item"><a href='documents/ElvisPresley.pdf'><img width='100' src='images/Elvis.jpg' alt='images'>Elvis Presley</a></li>
         <li class="list-group-item"><a href='documents/Ford.pdf'><img width='100' src='images/Ford.jpg' alt='images'>Ford</a></li>
-        <li class="list-group-item"><a href='documents/ILoveLucy.pdf'><img width='100' src='images/ILoveLucy.jpg' alt='images'>I Love Lucy</a></li>
+        <li class="list-group-item"><a href='documents/HarryPotter.pdf'><img width='100' src='images/HarryPotter.jpg' alt='images'>Harry Potter</a></li>
+        <li class="list-group-item"><a href='documents/HolidayMovies.pdf'><img width='100' src='images/HolidayMovies.jpg' alt='images'>Holiday Movies</a></li> <li class="list-group-item"><a href='documents/ILoveLucy.pdf'><img width='100' src='images/ILoveLucy.jpg' alt='images'>I Love Lucy</a></li>
         <li class="list-group-item"><a href='documents/LooneyTunes.pdf'><img width='100' src='images/LooneyTunes.jpg' alt='images'>Looney Tunes</a></li>
         <li class="list-group-item"><a href='documents/RadioDays.pdf'><img width='100' src='images/RadioDays.jpg' alt='images'>Radio Days</a></li>
         <li class="list-group-item"><a href='documents/ScoobyDoo.pdf'><img width='100' src='images/ScoobyDoo.jpg' alt='images'>Scooby Doo</a></li>
