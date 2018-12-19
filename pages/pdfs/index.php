7e55fa8c50d2414a8615050387a9b98d6b87a363
Nancy Snapper <nsnapper@spoontiques.com>
2018-12-18 19:36:16 -0500

    2019

--- pages/pdfs/index.php
+++ pages/pdfs/index.php
@@ -20,7 +20,7 @@
     <ul class="list-group">
         <li class="list-group-item"><a href='documents/FullCatalog.pdf'><img width='100' src='images/cover2.jpg' alt='images'>Spoontiques January 2019 Catalog</a></li>
 <!--        <li class="list-group-item"><a href='documents/Summer2018.pdf'><img width='100' src='images/Summer2018.jpg' alt='images'>New for Summer 2018</a></li>-->
-        <li class="list-group-item"><a href='sections.php'><img width='100' src='images/cover.jpg' alt='images'>Individual Sections</a></li>
+        <li class="list-group-item"><a href='sections.php'><img width='100' src='images/cover2.jpg' alt='images'>Individual Sections</a></li>
         <li class="list-group-item"><a href='licenses.php'><img width='100' src='images/licenses.jpg' alt='images'>Individual Licenses</a></li>
    </ul>    
    
