3c6c3998008fe4962d6b9088f1bbe3c12417ce57
Nancy Snapper <nsnapper@spoontiques.com>
2019-07-02 07:24:20 -0400

    canadian

--- pages/pdfs/canada.php
+++ pages/pdfs/canada.php
@@ -18,7 +18,7 @@
 <div id="pdf_lists">
     <ul class="list-group">
         <li class="list-group-item"><a href='documents/CanadianFullCatalog.pdf'><img width='100' src='images/cover.jpg' alt='images'>Spoontiques January 2019 Catalog with Canadian Pricing</a></li>
-<!--        <li class="list-group-item"><a href='documents/Summer2018Canadian.pdf'><img width='100' src='images/Summer2018.jpg' alt='images'>New for Summer 2018 with Canadian Pricing</a></li>-->
+        <li class="list-group-item"><a href='documents/CanadianSummer2019.pdf'><img width='100' src='images/Summer2018.jpg' alt='images'>New for Summer 2019 with Canadian Pricing</a></li>
         <li class="list-group-item"><a href='CanadianSections.php'><img width='100' src='images/cover.jpg' alt='images'>Individual Sections with Canadian Pricing</a></li>
         <li class="list-group-item"><a href='CanadianLicenses.php'><img width='100' src='images/licenses.jpg' alt='images'>Individual Licenses with Canadian Pricing</a></li>
    </ul>    
