d78cca5e8f6601c76860cc28e8227fe83af51d52
Nancy Snapper <nsnapper@spoontiques.com>
2020-05-29 08:25:56 -0400

    tax forms

--- pages/forms/TaxForms.php
+++ pages/forms/TaxForms.php
@@ -19,12 +19,26 @@
 <div id="pdf_lists">
     <ul class="list-group">
 
-        <li class="list-group-item"><a href='documents/D2002-Uniform-Sales-and-Use-Certificate-5-2016a-Spoontiques.pdf'>Multi-State Sales and Use Certificate</a></li>
-        <li class="list-group-item"><a href='documents/D2007-DC-2012_otr_368_fill-in.pdf'>District of Columbia Form OTR 368</a></li>
-        <li class="list-group-item"><a href='documents/D2008-IN-ST-105.pdf'>Indiana Form ST-105</a></li>
-        <li class="list-group-item"><a href='documents/D2004-MA-dor-sales-use-form-st-4.pdf'>Massachusetts Form ST-4</a></li>
-        <li class="list-group-item"><a href='documents/D2003-MI-3372_216612_7.pdf'>Michigan Form 3372</a></li>
-   </ul>    
+        <li class="list-group-item"><a href='documents/ZTX-Uniform-Sales-and-Use-Certificate-3-2019.pdf'>Multi-State Sales and Use Certificate</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-Streamlined-Sales-Tax-Agreement.pdf'>Streamlined Sales Tax Agreement</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-CA-cdtfa230.pdf'>California Form ST-105</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-CT-resalecert.pdf'>Connecticut Form ST-105</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-DC-2012_otr_368_fill-in.pdf'>District of Columbia Form OTR 368</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-IN-ST-105.pdf'>Indiana Form ST-105</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-KY-51A105.pdf'>Kentucky Form ST-105</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-LA-Resale-Certificate.pdf'>Louisiana Resale Certificate</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-MD-Blanket-Cert.pdf'>Maryland Form ST-105</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-MA-dor-sales-use-form-st-4.pdf'>Massachusetts Form ST-4</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-MI-3372_216612_7.pdf'>Michigan Form 3372</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-MS-Exempt-Affidavit.pdf'>Mississippi Exemption Affidavit</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-MT-Resale-Certificate.pdf'>Montana Resale Certificate</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-NY-120_fill_in.pdf'>New York Form 120</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-NC-595-e.pdf'>North Carolina 595-e</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-OR-business-registry-resale-cert_800-002.pdf'>Oregon Resale Certificate</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-PA-rev-1220.pdf'>Pennsylvania Form 1220</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-TX-01-339-front.pdf'>Texas Form 01-339</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-VA-ST-10.pdf'>Virginia Form ST-10</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-WY-ETS-Form-101.pdf'>Wyoming ETS Form 101</a></li>   </ul>    
    
 </div>
 
