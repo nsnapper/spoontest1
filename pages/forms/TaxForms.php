7667540df42b0d351a81e525169216eed24c56a6
Nancy Snapper <nsnapper@spoontiques.com>
2020-06-14 08:32:34 -0400

    tax forms

--- pages/forms/TaxForms.php
+++ pages/forms/TaxForms.php
@@ -20,25 +20,23 @@
     <ul class="list-group">
 
         <li class="list-group-item"><a href='documents/ZTX-Uniform-Sales-and-Use-Certificate-3-2019.pdf'>Multi-State Sales and Use Certificate</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-Streamlined-Sales-Tax-Agreement.pdf'>Streamlined Sales Tax Agreement</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-CA-cdtfa230.pdf'>California Form Resale Certificate</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-CT-resalecert.pdf'>Connecticut Resale Certificate</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-DC-2012_otr_368_fill-in.pdf'>District of Columbia Form OTR 368</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-IN-ST-105.pdf'>Indiana Form ST-105</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-KY-51A105.pdf'>Kentucky Form 51A105</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-LA-Resale-Certificate.pdf'>Louisiana Resale Certificate</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-MD-Blanket-Cert.pdf'>Maryland Form Blanket Certificate</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-MA-dor-sales-use-form-st-4.pdf'>Massachusetts Form ST-4</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-MI-3372_216612_7.pdf'>Michigan Form 3372</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-MS-Exempt-Affidavit.pdf'>Mississippi Exemption Affidavit</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-MT-Resale-Certificate.pdf'>Montana Resale Certificate</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-NY-120_fill_in.pdf'>New York Form 120</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-NC-595-e.pdf'>North Carolina 595-e</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-OR-business-registry-resale-cert_800-002.pdf'>Oregon Resale Certificate</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-PA-rev-1220.pdf'>Pennsylvania Form 1220</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-TX-01-339-front.pdf'>Texas Form 01-339</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-VA-ST-10.pdf'>Virginia Form ST-10</a></li>
-        <li class="list-group-item"><a href='documents/ZTX-WY-ETS-Form-101.pdf'>Wyoming ETS Form 101</a></li>   </ul>    
+        <li class="list-group-item"><a href='documents/ZTX-CA-CDTFA-230-0817.pdf'>California</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-CT-Resale-Cert.pdf'>Connecticut</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-DC-OTR-368-2012.pdf'>District of Columbia</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-IN-ST-105-0617.pdf'>Indiana</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-KY-51A105-0719.pdf'>Kentucky</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-LA-Form-R-1042-1008.pdf'>Louisiana</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-MD-Blanket-Cert.pdf'>Maryland</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-MA-Form-ST-4-0816.pdf'>Massachusetts</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-MI-Form-3372-0918.pdf'>Michigan</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-MS-Exempt-Affidavit.pdf'>Mississippi </a></li>
+        <li class="list-group-item"><a href='documents/ZTX-MT-Resale-Certificate.pdf'>Montana</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-NY-ST-120-0618.pdf'>New York</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-OR-150-800-002-0317.pdf'>Oregon</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-PA-REV-1220-0517.pdf'>Pennsylvania</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-TX-01-339-0413.pdf'>Texas</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-VA-ST-10-0417.pdf'>Virginia</a></li>
+        <li class="list-group-item"><a href='documents/ZTX-WY-ETS-Form-101.pdf'>Wyoming</a></li>   </ul>    
    
 </div>
 
