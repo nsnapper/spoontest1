1e3de7bbed66abe3643dbe9b2a77a59c2a2dd7fa
Nancy Snapper <nsnapper@spoontiques.com>
2020-08-18 12:49:35 -0400

    ppe

--- pages/pdfs/canada.php
+++ pages/pdfs/canada.php
@@ -20,9 +20,9 @@
         <li class="list-group-item"><a href='documents/CanadianFullCatalog.pdf'><img width='100' src='images/cover2.jpg' alt='images'>Spoontiques January 2020 Catalog with Canadian Pricing</a></li>
         <li class="list-group-item"><a href='CanadianSections.php'><img width='100' src='images/cover2.jpg' alt='images'>Individual Sections with Canadian Pricing</a></li>
         <li class="list-group-item"><a href='CanadianLicenses.php'><img width='100' src='images/licenses.jpg' alt='images'>Individual Licenses with Canadian Pricing</a></li>
-        <li class="list-group-item"><a href='documents/CanadianSummer2020.pdf'><img width='100' src='images/Summer2020.jpg' alt='images'>New for Summer 2020 with Canadian Pricing</a></li>
-        <li class="list-group-item"><a href='documents/CanadianHoliday2020FaceMasks.pdf'><img width='100' src='images/CanadianHoliday2020FaceMasks.jpg' alt='images'>Holiday Face Masks - Available Oct, 1, 2020</a></li>
-        <li class="list-group-item"><a href='documents/CanadianHoliday2020HairWraps.pdf'><img width='100' src='images/CanadianHoliday2020HairWraps.jpg' alt='images'>Holiday Hair and Face Wraps - Available Oct, 1, 2020</a></li>
+        <li class="list-group-item"><a href='documents/CanadianSummer2020.pdf'><img width='100'
+        <li class="list-group-item"><a href='documents/CanadianFall2020PPE.pdf'><img width='100' src='images/PPE.jpg' alt='images'>Personal Protection</a></li>
+
    </ul>    
    
 </div>
