1d3bda9a330c8910590b030d919b8324db0fa9f5
Bill Snapper <bill@snapper.net>
2021-09-30 15:33:58 -0400

    Added production root to app_variables.

--- app_variables.php
+++ app_variables.php
@@ -2,7 +2,8 @@
 
 // TODO: Change app_root_dir to come from a server environment variable.
 //      This will make it easier to use in dev / production.
-$app_root_dir = '/~bill/spoontest';
+$app_root_dir = '/'; // Production root
+// $app_root_dir = '/~bill/spoontest'; // Bill Devel Server
 
 $pdf_file_dir_path = "pages/pdfs/docs";
 $pdf_file_dir = "$app_root_dir/$pdf_file_dir_path";
