12db0721ec6463be09af12d9e2fb529700bad804
Nancy Snapper <nsnapper@spoontiques.com>
2023-10-29 13:57:50 -0400

    Add Tradeshow Page

---
+++ tradeshows.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <?php include "includes/common_head.php"; ?>
+    
+</head>
+
+    
+ 
+ <body>
+     
+<?php include "includes/logo.php"; ?>
+<?php include "includes/navbar.php"; ?>  
+
+<divid="TradeShows">
+
+    <div class="row">
+         <p class="subtitle">Trade Shows</p>
+    </div>
+
+</div> 
+            
+<divid="Atlanta">
+
+    <div class="row">
+         <p class="subtitle">Atlanta</p>
+    </div>
+
+</div> 
+
+   <div id="Note">
+    <p class="pageNote">Please visit us at booth 123. </p>
+</div>
+            
+
+            
+<div id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="card">
+    <div class="card-header" role="tab" id="heading01">
+      <h5 class="mb-0" >
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse01" aria-expanded="true" aria-controls="collapse01" style="color:lightseagreen">
+            Atlanta
+        </a>
+      </h5>
+    </div>
+
+    <div id="collapse01" class="collapse in" role="tabpanel" aria-labelledby="heading01">
+      <div class="card-block">
+        Anne McGilvray. asklfjsafjweiposkdfjkdsjf;lkasdjfa
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingTwo">
+      <h5 class="mb-0">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"  style="color:lightseagreen">
+          Dallas	
+
+        </a>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="card-block">
+        Blah blah blah.
+    </div>
+  </div>
+    </div>
+<!--
+  <div class="card">
+    <div class="card-header" role="tab" id="headingThree">
+      <h5 class="mb-0">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree"  style="color:lightseagreen">
+          Do you accept credit cards?
+        </a>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="card-block">
+       We accept Visa, Master Card, American Express and Discover.
+      </div>
+    </div>
+  </div>
+-->
+  <div class="card">
+    <div class="card-header" role="tab" id="headingFour">
+      <h5 class="mb-0">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour"  style="color:lightseagreen">
+          	Las Vegas
+        </a>
+      </h5>
+    </div>
+    <div id="collapseFour" class="collapse" role="tabpanel" aria-labelledby="headingFour">
+      <div class="card-block">
+       Yada yada yada.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingSix">
+      <h5 class="mb-0">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix"  style="color:lightseagreen">
+          	How can I receive a current catalog?
+        </a>
+      </h5>
+    </div>
+    <div id="collapseSix" class="collapse" role="tabpanel" aria-labelledby="headingSix">
+      <div class="card-block">
+          Fill out our online <a href="CatRequest.php">catalog request</a> form or call our Customer Service Department at 800-225-5826. 
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="heading07">
+      <h5 class="mb-0">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse07" aria-expanded="false" aria-controls="collapse07"  style="color:lightseagreen">
+          Do you have any showrooms?
+        </a>
+      </h5>
+    </div>
+    <div id="collapse07" class="collapse" role="tabpanel" aria-labelledby="heading07">
+      <div class="card-block">
+          Yes, we have several located across the US and Canada. 
+          <a href="ContactUs.php">Contact us</a> online or call our Customer Service Department at 800-225-5826 for the closest one to you.
+      </div>
+    </div>
+  </div>
+ 
+<!--
+  <div class="card">
+    <div class="card-header" role="tab" id="heading09">
+      <h5 class="mb-0">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse09" aria-expanded="false" aria-controls="collapse09"  style="color:lightseagreen">
+         	Are you interested in suggestions for new products?
+        </a>
+      </h5>
+    </div>
+    <div id="collapse09" class="collapse" role="tabpanel" aria-labelledby="heading09">
+      <div class="card-block">
+       Of course. Some of our best sellers started with suggestions from our customers.
+      </div>
+    </div>
+  </div>
+-->
+</div>
+		
+<?php include "includes/footer.php"; ?>
+
+
+  </body>
+    
+</html>
