abe6cc13cf7ce8c66fb24884f13e8ece1506e41c
Nancy Snapper <nsnapper@spoontiques.com>
2023-12-27 13:56:09 -0500

    Trade Shows

--- tradeshows.php
+++ tradeshows.php
@@ -19,131 +19,59 @@
     <div class="row">
          <p class="subtitle">Trade Shows</p>
     </div>
+       
+    <div class="row">
+         <p class="show-name">Atlanta</p>
+    </div>
 
-</div> 
-            
-<divid="Atlanta">
+    <div id="Note">
+        <p class="show-desc">Anne McGilvray & Company</p>
+        <p class="show-desc">Americas Mart – Building 2</p>
+        <p class="show-desc">Suite #1718</p>
+    </div
+    
+    <div class="row">
+         <p class="show-name">Chicago</p>
+    </div>
+
+    <div id="Note">
+        <p class="show-desc">DNA Sales</p>
+        <p class="show-desc">Midwest Market Exchange</p>
+        <p class="show-desc">Suite #184</p>
+    </div
 
     <div class="row">
-         <p class="subtitle">Atlanta</p>
+         <p class="show-name">Dallas</p>
     </div>
+
+    <div id="Note">
+        <p class="show-desc">Anne McGilvray & Company</p>
+        <p class="show-desc">World Trade Center</p>
+        <p class="show-desc">Suite #270</p>
+    </div
+
+    <div class="row">
+         <p class="show-name">Las Vegas</p>
+    </div>
+
+    <div id="Note">
+        <p class="show-desc">Sales Producers, Inc</p>
+        <p class="show-desc">World Market Center – Building C</p>
+        <p class="show-desc">Suite #1044</p>
+    </div
+
+    <div class="row">
+         <p class="show-name">Toronto</p>
+    </div>
+
+    <div id="Note">
+        <p class="show-desc">Auralim Gift</p>
+        <p class="show-desc">Show T.O. Home & Gift</p>
+        <p class="show-desc">Suite #214</p>
+    </div
 
 </div> 
 
-   <div id="Note">
-    <p class="pageNote">Please visit us at booth 123. </p>
-</div>
-            
-
-            
-<div id="accordion" role="tablist" aria-multiselectable="true">
-  <div class="card">
-    <div class="card-header" role="tab" id="heading01">
-      <h5 class="mb-0" >
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse01" aria-expanded="true" aria-controls="collapse01" style="color:lightseagreen">
-            Atlanta
-        </a>
-      </h5>
-    </div>
-
-    <div id="collapse01" class="collapse in" role="tabpanel" aria-labelledby="heading01">
-      <div class="card-block">
-        Anne McGilvray. asklfjsafjweiposkdfjkdsjf;lkasdjfa
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" role="tab" id="headingTwo">
-      <h5 class="mb-0">
-        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"  style="color:lightseagreen">
-          Dallas	
-
-        </a>
-      </h5>
-    </div>
-    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
-      <div class="card-block">
-        Blah blah blah.
-    </div>
-  </div>
-    </div>
-<!--
-  <div class="card">
-    <div class="card-header" role="tab" id="headingThree">
-      <h5 class="mb-0">
-        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree"  style="color:lightseagreen">
-          Do you accept credit cards?
-        </a>
-      </h5>
-    </div>
-    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
-      <div class="card-block">
-       We accept Visa, Master Card, American Express and Discover.
-      </div>
-    </div>
-  </div>
--->
-  <div class="card">
-    <div class="card-header" role="tab" id="headingFour">
-      <h5 class="mb-0">
-        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour"  style="color:lightseagreen">
-          	Las Vegas
-        </a>
-      </h5>
-    </div>
-    <div id="collapseFour" class="collapse" role="tabpanel" aria-labelledby="headingFour">
-      <div class="card-block">
-       Yada yada yada.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" role="tab" id="headingSix">
-      <h5 class="mb-0">
-        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="false" aria-controls="collapseSix"  style="color:lightseagreen">
-          	How can I receive a current catalog?
-        </a>
-      </h5>
-    </div>
-    <div id="collapseSix" class="collapse" role="tabpanel" aria-labelledby="headingSix">
-      <div class="card-block">
-          Fill out our online <a href="CatRequest.php">catalog request</a> form or call our Customer Service Department at 800-225-5826. 
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" role="tab" id="heading07">
-      <h5 class="mb-0">
-        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse07" aria-expanded="false" aria-controls="collapse07"  style="color:lightseagreen">
-          Do you have any showrooms?
-        </a>
-      </h5>
-    </div>
-    <div id="collapse07" class="collapse" role="tabpanel" aria-labelledby="heading07">
-      <div class="card-block">
-          Yes, we have several located across the US and Canada. 
-          <a href="ContactUs.php">Contact us</a> online or call our Customer Service Department at 800-225-5826 for the closest one to you.
-      </div>
-    </div>
-  </div>
- 
-<!--
-  <div class="card">
-    <div class="card-header" role="tab" id="heading09">
-      <h5 class="mb-0">
-        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse09" aria-expanded="false" aria-controls="collapse09"  style="color:lightseagreen">
-         	Are you interested in suggestions for new products?
-        </a>
-      </h5>
-    </div>
-    <div id="collapse09" class="collapse" role="tabpanel" aria-labelledby="heading09">
-      <div class="card-block">
-       Of course. Some of our best sellers started with suggestions from our customers.
-      </div>
-    </div>
-  </div>
--->
-</div>
 		
 <?php include "includes/footer.php"; ?>
 
